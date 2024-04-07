71227077a461de37eeeb2051093d4a9ba444f3bd
blytle1 <s-blytle1@huskers.unl.edu>
2024-04-07 13:17:24 -0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./globals.css";
+import Link from "next/link";
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Home</h1>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-      />
+      <Link href="/About">About</Link>
     </div>
   );
 }
