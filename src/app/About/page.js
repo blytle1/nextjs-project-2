b931934d7f7646f3a84b3f168c6991eff03047f3
blytle1 <s-blytle1@huskers.unl.edu>
2024-04-07 17:13:07 -0500

    updates

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
       <Link href="/">Home</Link>
+      <Link href="/Contact">Contact</Link>
+      <Link href="/Services">Services</Link>
+      <Link href="/Products">Products</Link>
+      <h2>About Page</h2>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
       <Link href="/About">About</Link>
+      <Link href="/Contact">Contact</Link> 
+      <Link href="/Services">Services</Link>
+      <Link href="/Products">Products</Link>
+      <h2>Home Page</h2>
+      <p>Welcome to the home page</p>
+      <button>Click me</button>
     </div>
   );
 }
