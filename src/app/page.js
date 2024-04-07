15afbdcd94391f0f23c973f85aea58931c00a56c
blytle1 <s-blytle1@huskers.unl.edu>
2024-04-07 13:42:21 -0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
-import styles from "./globals.css";
 import Link from "next/link";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
-
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>Home</h1>
       <Link href="/About">About</Link>
     </div>
   );
 }
+
+
