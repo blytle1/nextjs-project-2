89200795942450a07e4c1f0fdd340af93372a7c1
blytle1 <s-blytle1@huskers.unl.edu>
2024-04-12 22:51:43 -0500

    update style

--- src/app/Components/header.js
+++ src/app/Components/header.js
@@ -4,9 +4,9 @@ import styles from './header.modules.css';
 function Header() {
   return (
     <nav className="nav">
-      <h1>Welcome To The Assistive Technology Assessment Tool</h1>
+      <h1 className="site-title">Welcome To The Assistive Technology Assessment Tool</h1>
         <ul>
-          <li><a href="/" className="site-title">Home</a></li>
+          <li><a href="/">Home</a></li>
           <li><a href="/assessment">Assessment Tool</a></li>
           <li><a href="/TechnologyTools">Assistive Technology Tools</a></li>
           <li><a href="/ContactInfo">Contact Information</a></li>  
