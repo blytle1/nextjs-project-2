543ed9de2b33c06c85aaf63ea6be5b82a896f612
blytle1 <s-blytle1@huskers.unl.edu>
2024-04-13 08:29:06 -0500

    update header

--- src/app/Components/header.js
+++ src/app/Components/header.js
@@ -6,7 +6,7 @@ function Header() {
     <nav className="nav">
       <h1 className="site-title">Welcome To The Assistive Technology Assessment Tool</h1>
         <ul>
-          <li><a href="/">Home</a></li>
+          <li className='active'><a href="/">Home</a></li>
           <li><a href="/assessment">Assessment Tool</a></li>
           <li><a href="/TechnologyTools">Assistive Technology Tools</a></li>
           <li><a href="/ContactInfo">Contact Information</a></li>  
