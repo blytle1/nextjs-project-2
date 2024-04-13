3e3cdbfce17834e1ab0af7744784aa1067d3d4d1
blytle1 <s-blytle1@huskers.unl.edu>
2024-04-13 14:14:24 -0500

    style

--- src/app/Components/header.js
+++ src/app/Components/header.js
@@ -1,21 +1,16 @@
-'use client';
-import React from 'react';
 import styles from './header.modules.css';
 
 function Header() {
-  // Function to determine if a given link is active
-  const isActive = (pathname) => {
-    return window.location.pathname === pathname;
-  };
+
 
   return (
     <nav className="nav">
       <h1 className="site-title">Welcome To The Assistive Technology Assessment Tool</h1>
       <ul>
-        <li className={isActive("/") ? 'active' : ''}><a href="/">Home</a></li>
-        <li className={isActive("/assessment") ? 'active' : ''}><a href="/assessment">Assessment Tool</a></li>
-        <li className={isActive("/TechnologyTools") ? 'active' : ''}><a href="/TechnologyTools">Assistive Technology Tools</a></li>
-        <li className={isActive("/ContactInfo") ? 'active' : ''}><a href="/ContactInfo">Contact Information</a></li>  
+        <li><a href="/">Home</a></li>
+        <li><a href="/assessment">Assessment Tool</a></li>
+        <li><a href="/TechnologyTools">Assistive Technology Tools</a></li>
+        <li><a href="/ContactInfo">Contact Information</a></li>  
       </ul>
     </nav>
   );
