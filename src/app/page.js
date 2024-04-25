eaebe0c237b88a881d1cee6aacdf819e2b7c9ac4
blytle1 <s-blytle1@huskers.unl.edu>
2024-04-25 10:00:26 -0500

    image update

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,48 @@
 import Header from './Components/header';   
+import React from 'react'; // 
+import Image from 'next/image';
+import formImage from './images/braillesense.png';
+import formImage2 from './images/qbraille.png'; 
+
+const imageData = [
+  {link: 'https://hims-inc.com/product/braillesense-6/' }, 
+];
+const imageData2 = [
+  {link: 'https://hims-inc.com/product/qbraille-xl/' }, 
+];
 
 export default function Home() {
   return (
     <div>
       <Header />
       <h2>Home Page</h2>
-      <p>Welcome to the home page</p>
-      <button>Click me</button>
+      <h3>Welcome To The Home Page!</h3>
+      <h3>Please use the Navigation Bar at the top to access the different pages of this web tool application.</h3>
+      <div>
+        <p>This web tool application is designed to best support teachers of students with visual impairments (TSVIs).
+          Through the use of this tool, TSVIs can better prepare for their students' assistive technology (AT) needs.
+          The Assessment Tool link at the top will direct you to the assessment tool page where you can input your student's information and receive a list of AT recommendations.
+          Additionally, please make sure and fill out the Contact Information page so that we can better assist you with any questions or concerns you may have.
+          The tool is free to use, and we are always looking for ways to improve it. Please feel free to reach out to us with any feedback you may have.
+          Below are some helpful links to resources that may be of use to you.  Click on the various images to take you directly to the manufacturer's website.
+        </p>
+      </div>
+      <div className="image-container">
+        {imageData.map((item, index) => ( 
+          <div key={index} className="image-wrapper">
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <Image className="img1" src={formImage} alt={`Image ${index}`} width={375} height={200} />
+            </a>
+          </div>
+        ))}
+        {imageData2.map((item, index) => ( 
+          <div key={index} className="image-wrapper">
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <Image className="img2" src={formImage2} alt={`Image ${index}`} width={375} height={200} />
+            </a>
+          </div>
+        ))}
+    </div>
     </div>
   );
 }
-
-
-
